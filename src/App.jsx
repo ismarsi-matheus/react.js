96e1984215dcf7362ea1c7129393963ad3f43586
ismarsi-matheus <matheusleo.senac@gmail.com>
2025-09-22 19:15:26 -0300

    estilização inicial com o tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import AddTask from "./components/AddTasks"
 import Tasks from "./components/Tasks"
+import './index.css'
+
 
 
 function App() {
 
   return (
-    <div>
-      <h1>Gerenciador de Tarefas</h1>
-      <AddTask />
-      <Tasks />
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center"> Gerenciador de Tarefas</h1>
+        <AddTask />
+        <Tasks />
+      </div>
     </div>
   )
 }
