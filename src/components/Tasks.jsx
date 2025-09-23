f9bff03d3aaaf8626d657c777c76c160c14f6b99
ismarsi-matheus <matheusleo.senac@gmail.com>
2025-09-23 12:11:21 -0300

    função com props

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -6,9 +6,11 @@ function Tasks(props) {
         <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
             {props.tasks.map((task) => (
                 <li key={task.id} className="flex gap-2">
-                    <button className="bg-slate-400 w-full text-left text-white p-2 rounded-md">{task.title}</button>
+                    <button onClick={() => props.onTaskClick(task.id)} className="bg-slate-400 w-full text-left text-white p-2 rounded-md">{task.title}
+                        {task.isCompleted ?"COMPLETE": "INCOMPLETE"}
+                    </button>
                     <button className="bg-slate-400 p-2 rounded-md text-white">
-                        <ChevronRightIcon /> 
+                        <ChevronRightIcon />
                     </button>
                 </li>
             ))}
