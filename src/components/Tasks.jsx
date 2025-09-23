c911d0cbf1e4760177a84f4b1cac9615ae7a1772
ismarsi-matheus <matheusleo.senac@gmail.com>
2025-09-23 16:32:38 -0300

    navegação de páginas

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,17 +1,36 @@
 import { ChevronRightIcon, TrashIcon } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
-function Tasks({tasks, onTaskClick, onDeleteTaskClick}) {
+function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
+    const navigate = useNavigate();
+
+    function onSeeDetailsClick(task) {
+        navigate(`/task?title=${task.title}&description=${task.description}`);
+    }
+
     return (
         <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
             {tasks.map((task) => (
                 <li key={task.id} className="flex gap-2">
-                    <button onClick={() => onTaskClick(task.id)} className={`bg-slate-400 w-full text-left text-white p-2 rounded-md ${task.isCompleted && "line-through"}`}>{task.title}
-
+                    <button
+                        onClick={() => onTaskClick(task.id)}
+                        className={`bg-slate-400 w-full text-left text-white p-2 rounded-md ${task.isCompleted && "line-through"
+                            }`}
+                    >
+                        {task.title}
                     </button>
-                    <button className="bg-slate-400 p-2 rounded-md text-white">
+
+                    <button
+                        onClick={() => onSeeDetailsClick(task)}
+                        className="bg-slate-400 p-2 rounded-md text-white"
+                    >
                         <ChevronRightIcon />
                     </button>
-                    <button onClick={() => onDeleteTaskClick(task.id)} className="bg-slate-400 p-2 rounded-md text-white">
+
+                    <button
+                        onClick={() => onDeleteTaskClick(task.id)}
+                        className="bg-slate-400 p-2 rounded-md text-white"
+                    >
                         <TrashIcon />
                     </button>
                 </li>
@@ -19,4 +38,5 @@ function Tasks({tasks, onTaskClick, onDeleteTaskClick}) {
         </ul>
     );
 }
+
 export default Tasks;
