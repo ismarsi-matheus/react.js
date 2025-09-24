24af3855af6315dead267c9bad15701dc17a9108
ismarsi-matheus <matheusleo.senac@gmail.com>
2025-09-24 01:31:29 -0300

    Criando o componente input

---
+++ src/components/Input.jsx
@@ -0,0 +1,13 @@
+function input(props) {
+    return (
+        <input
+
+            className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+            {...props}
+
+        />
+    )
+}
+
+
+export default input;
